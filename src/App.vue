e0e78febfcbda70d1e481d815bb560ae755c7536
Maadhav Bhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2025-11-16 17:16:25 +0530

    style: add reset and base layer, set up CSS variables, and change tab title in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,143 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <section class="container">
+      <h1>You did it!</h1>
+      <p>
+        Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+        documentation
+      </p>
+    </section>
+  </main>
 </template>
 
-<style scoped></style>
+<style>
+/* Font imports */
+@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+
+
+:root {
+  --font-primary: 'Inter', system-ui, -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif,
+    'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+
+  --clr-black-1: #121212;
+  --clr-black-2: #333;
+  --clr-white-1: #eee;
+
+  --clr-text: var(--clr-black-2);
+  --clr-bg: var(--clr-white-1);
+}
+
+/* Do not use !important inside layers */
+@layer reset, base;
+
+/* CSS Reset from https://www.joshwcomeau.com/css/custom-css-reset/ */
+@layer reset {
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    html {
+      interpolate-size: allow-keywords;
+    }
+  }
+
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+  }
+
+  p {
+    text-wrap: pretty;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    text-wrap: balance;
+  }
+
+  #root,
+  #__next {
+    isolation: isolate;
+  }
+
+  :root {
+    font-size: 10px;
+  }
+}
+
+@layer base {
+  body {
+    font-family: var(--font-primary);
+    font-optical-sizing: auto;
+
+    color: var(--clr-text);
+    background-color: var(--clr-bg);
+
+    min-height: 100vh;
+  }
+
+  .container {
+    max-width: 1200px;
+    width: 80%;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+  }
+
+  a {
+    text-decoration: underline;
+    color: inherit;
+
+    &:hover,
+    &:focus {
+      color: blue;
+    }
+  }
+
+  h1 {
+    font-size: 10rem;
+  }
+}
+</style>
